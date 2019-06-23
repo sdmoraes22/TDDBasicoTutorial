b6d0a83bc1662662b9e87dd2c560be72cf412285
Cristiano Moraes <cmoraes.ti@gmail.com>
2019-06-23 16:48:21 -0300

    Adicionar arquivos de projeto.

---
+++ WebTDD/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebTDD.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
